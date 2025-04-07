4a9421ac3c0092e3c66cd9f5a7384420c38025c0
Arturo Gonzalez Martinez <arturogonzalez1989@gmail.com>
2025-04-08 00:03:54 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ArturoGonzalez1989.github.io',
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
